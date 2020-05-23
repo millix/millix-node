8113d0b7288dd0a0551a52ef88a582910178d038
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2020-05-23 14:20:06 +0100

    improve node shard_attribute update on boot.

--- core/serices/services.js
+++ core/serices/services.js
@@ -6,7 +6,7 @@ import jobEngine from '../../job/job-engine';
 import console from '../console';
 import logManager from '../log-manager';
 import database from '../../database/database';
-import genesisConfig from '../../core/genesis/genesis-config';
+import _ from 'lodash';
 import ntp from '../ntp';
 
 
@@ -32,16 +32,20 @@ class Service {
                          .then(() => jobEngine.initialize())
                          .then(() => {
                              return new Promise(resolve => {
-                                 const nodeRepository = database.getRepository('node');
-                                 nodeRepository.addNodeAttribute(network.nodeID, 'shard_protocol', JSON.stringify([
-                                     {
-                                         'shard_id'            : genesisConfig.genesis_shard_id,
-                                         'transaction_count'   : 0,
-                                         'update_date'         : Math.floor(ntp.now().getTime() / 1000),
-                                         'is_assigned'         : true,
-                                         'fee_ask_request_byte': 20
-                                     }
-                                 ])).then(() => resolve()).catch(() => resolve());
+                                 const nodeRepository  = database.getRepository('node');
+                                 const shardRepository = database.getRepository('shard');
+                                 shardRepository.listShard()
+                                                .then(shardList => {
+                                                    const shardAttributeList = [];
+                                                    _.each(shardList, shard => shardAttributeList.push({
+                                                        'shard_id'            : shard.shard_id,
+                                                        'transaction_count'   : 0,
+                                                        'update_date'         : Math.floor(ntp.now().getTime() / 1000),
+                                                        'is_assigned'         : true,
+                                                        'fee_ask_request_byte': 20
+                                                    }));
+                                                    return nodeRepository.addNodeAttribute(network.nodeID, 'shard_protocol', JSON.stringify(shardAttributeList));
+                                                }).then(() => resolve()).catch(() => resolve());
                              });
                          })
                          .then(() => wallet.setMode(this.mode).initialize(initializeWalletEvent))
