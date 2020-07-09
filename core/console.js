9edbaa887e4cc9685930c026bc8d7f732ee9635a
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2020-07-09 20:19:18 +0100

    improve millix-node run from script. save node_id and node_signature to node.js at millix-node's data folder.

--- core/console.js
+++ core/console.js
@@ -1,27 +1,10 @@
 import config from './config/config';
-
-let enabled       = true;
-console.disable   = () => enabled = false;
-console.enable    = () => enabled = true;
+let enabled = true;
+console.disable = () => enabled = false;
+console.enable = () => enabled = true;
 const _consoleLog = console.log;
-const filters     = [];
-
-console.addFilter = function(filter) {
-    filters.push(filter);
+console.log      = function() {
+    enabled && config.MODE_DEBUG && _consoleLog.apply(console, arguments);
 };
-
-console.log = function() {
-    if (!enabled || !config.MODE_DEBUG) {
-        return;
-    }
-    let showLog = true;
-    if (filters.length > 0) {
-        const regex = new RegExp(`^\\[(${filters.join('|')})[^\\]]*\\]`, 'm');
-        showLog     = !!regex.exec(arguments[0]);
-    }
-    showLog && _consoleLog.apply(console, arguments);
-};
-
-config.DEBUG_LOG_FILTER.forEach(filter => console.addFilter(filter));
 
 export default console;
