434243dc7a65c38148ec18f6eb0a9d996c742c79
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2020-08-06 12:11:57 +0100

    reset test network genesis.

--- core/genesis/genesis-config.js
+++ core/genesis/genesis-config.js
@@ -1,6 +1,6 @@
 import config from '../config/config';
 
 export default {
-    genesis_transaction: config.MODE_TEST_NETWORK ? 'z83hpG6RVcyUwq6nFsw1jiMgPLhdoMabiNQX8rNW5rrwMEfDy' : '2VngVznbdiQ5tqfWqn2NMP8DijqCbLX79Gygo9yYRVFU6iN35h',
-    genesis_shard_id   : config.MODE_TEST_NETWORK ? 'UqnsGsRQGtxK3z5yURxt23MX2A7u5wAsMmTvYqrzqACD2W4ne' : 'qGuUgMMVmaCvqrvoWG6zARjkrujGMpzJmpNhBgz1y3RjBG7ZR'
+    genesis_transaction: config.MODE_TEST_NETWORK ? '2sDCsRbDCaM3znBSC9bXcwWPdYdPi6uYTXQYb8R8uikn7mvad4' : '2VngVznbdiQ5tqfWqn2NMP8DijqCbLX79Gygo9yYRVFU6iN35h',
+    genesis_shard_id   : config.MODE_TEST_NETWORK ? 'PjqVBYLaKUpb9Cpfi6V6rLZjs9QejTUPuop2b62Nvrg51aC7s' : 'qGuUgMMVmaCvqrvoWG6zARjkrujGMpzJmpNhBgz1y3RjBG7ZR'
 };
