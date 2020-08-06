21c12117a596d6308e874aa3b48c3a39ee0d890d
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2020-08-06 13:02:16 +0100

    add list of supported transaction versions. reset test network genesis.

--- core/genesis/genesis-config.js
+++ core/genesis/genesis-config.js
@@ -1,6 +1,6 @@
 import config from '../config/config';
 
 export default {
-    genesis_transaction: config.MODE_TEST_NETWORK ? '2sDCsRbDCaM3znBSC9bXcwWPdYdPi6uYTXQYb8R8uikn7mvad4' : '2VngVznbdiQ5tqfWqn2NMP8DijqCbLX79Gygo9yYRVFU6iN35h',
-    genesis_shard_id   : config.MODE_TEST_NETWORK ? 'PjqVBYLaKUpb9Cpfi6V6rLZjs9QejTUPuop2b62Nvrg51aC7s' : 'qGuUgMMVmaCvqrvoWG6zARjkrujGMpzJmpNhBgz1y3RjBG7ZR'
+    genesis_transaction: config.MODE_TEST_NETWORK ? 'wFtfcYFJb75bJCGU3HVsPCRg8RDHnMmsRL565BSaeu3aidqQN' : '2VngVznbdiQ5tqfWqn2NMP8DijqCbLX79Gygo9yYRVFU6iN35h',
+    genesis_shard_id   : config.MODE_TEST_NETWORK ? '2qkemPc9SozT4r8TdcMgyv9DxgE8m4qUkEJ2eBRQLCNP9GD4Vh' : 'qGuUgMMVmaCvqrvoWG6zARjkrujGMpzJmpNhBgz1y3RjBG7ZR'
 };
