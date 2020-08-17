6328a0a37b81900d5bdfb0256f4db32203ba55e4
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2020-08-17 23:52:37 +0100

    update test network configs

--- core/bootstrap.js
+++ core/bootstrap.js
@@ -2,9 +2,16 @@ import fs from 'fs';
 import config, {
     DATA_BASE_DIR_TEST_NETWORK, DATA_BASE_DIR_MAIN_NETWORK,
     NODE_INITIAL_LIST_MAIN_NETWORK, NODE_INITIAL_LIST_TEST_NETWORK,
-    NODE_PORT as NODE_PORT_MAIN_NETWORK, NODE_PORT as NODE_PORT_TEST_NETWORK,
-    WALLET_TRANSACTION_DEFAULT_VERSION as WALLET_TRANSACTION_DEFAULT_VERSION_MAIN_NETWORK, WALLET_TRANSACTION_DEFAULT_VERSION as WALLET_TRANSACTION_DEFAULT_VERSION_TEST_NETWORK,
+    NODE_PORT_MAIN_NETWORK, NODE_PORT_TEST_NETWORK,
+    WALLET_TRANSACTION_DEFAULT_VERSION_MAIN_NETWORK, WALLET_TRANSACTION_DEFAULT_VERSION_TEST_NETWORK,
+    WALLET_TRANSACTION_REFRESH_VERSION_MAIN_NETWORK, WALLET_TRANSACTION_REFRESH_VERSION_TEST_NETWORK,
+    WALLET_TRANSACTION_SUPPORTED_VERSION_MAIN_NETWORK, WALLET_TRANSACTION_SUPPORTED_VERSION_TEST_NETWORK
 } from './config/config';
+
+import genesisConfig, {
+    GENESIS_SHARD_ID_MAIN_NETWORK, GENESIS_SHARD_ID_TEST_NETWORK,
+    GENESIS_TRANSACTION_MAIN_NETWORK, GENESIS_TRANSACTION_TEST_NETWORK
+} from './genesis/genesis-config';
 
 
 class _Bootstrap {
@@ -27,21 +34,25 @@ class _Bootstrap {
             return;
         }
 
-        config['MODE_TEST_NETWORK']                  = isModeTestNetwork;
-        config['NODE_PORT']                          = isModeTestNetwork ? NODE_PORT_TEST_NETWORK : NODE_PORT_MAIN_NETWORK;
-        config['NODE_INITIAL_LIST']                  = isModeTestNetwork ? NODE_INITIAL_LIST_TEST_NETWORK : NODE_INITIAL_LIST_MAIN_NETWORK;
-        config['WALLET_TRANSACTION_DEFAULT_VERSION'] = isModeTestNetwork ? WALLET_TRANSACTION_DEFAULT_VERSION_TEST_NETWORK : WALLET_TRANSACTION_DEFAULT_VERSION_MAIN_NETWORK;
-
-        let DATA_BASE_DIR                   = isModeTestNetwork ? DATA_BASE_DIR_TEST_NETWORK : DATA_BASE_DIR_MAIN_NETWORK;
-        config['NODE_KEY_PATH']             = DATA_BASE_DIR + '/node.json';
-        config['WALLET_KEY_PATH']           = DATA_BASE_DIR + '/millix_private_key.json';
-        config['JOB_CONFIG_PATH']           = DATA_BASE_DIR + '/job.json';
-        config['NODE_CERTIFICATE_KEY_PATH'] = DATA_BASE_DIR + '/node_certificate_key.pem';
-        config['NODE_CERTIFICATE_PATH']     = DATA_BASE_DIR + '/node_certificate.pem';
+        config['MODE_TEST_NETWORK']                    = isModeTestNetwork;
+        config['NODE_PORT']                            = isModeTestNetwork ? NODE_PORT_TEST_NETWORK : NODE_PORT_MAIN_NETWORK;
+        config['NODE_INITIAL_LIST']                    = isModeTestNetwork ? NODE_INITIAL_LIST_TEST_NETWORK : NODE_INITIAL_LIST_MAIN_NETWORK;
+        config['WALLET_TRANSACTION_DEFAULT_VERSION']   = isModeTestNetwork ? WALLET_TRANSACTION_DEFAULT_VERSION_TEST_NETWORK : WALLET_TRANSACTION_DEFAULT_VERSION_MAIN_NETWORK;
+        config['WALLET_TRANSACTION_REFRESH_VERSION']   = isModeTestNetwork ? WALLET_TRANSACTION_REFRESH_VERSION_TEST_NETWORK : WALLET_TRANSACTION_REFRESH_VERSION_MAIN_NETWORK;
+        config['WALLET_TRANSACTION_SUPPORTED_VERSION'] = isModeTestNetwork ? WALLET_TRANSACTION_SUPPORTED_VERSION_TEST_NETWORK : WALLET_TRANSACTION_SUPPORTED_VERSION_MAIN_NETWORK;
+        let DATA_BASE_DIR                              = isModeTestNetwork ? DATA_BASE_DIR_TEST_NETWORK : DATA_BASE_DIR_MAIN_NETWORK;
+        config['NODE_KEY_PATH']                        = DATA_BASE_DIR + '/node.json';
+        config['WALLET_KEY_PATH']                      = DATA_BASE_DIR + '/millix_private_key.json';
+        config['JOB_CONFIG_PATH']                      = DATA_BASE_DIR + '/job.json';
+        config['NODE_CERTIFICATE_KEY_PATH']            = DATA_BASE_DIR + '/node_certificate_key.pem';
+        config['NODE_CERTIFICATE_PATH']                = DATA_BASE_DIR + '/node_certificate.pem';
 
         if (config.DATABASE_ENGINE === 'sqlite') {
             config['DATABASE_CONNECTION'].FOLDER = DATA_BASE_DIR + '/';
         }
+
+        genesisConfig['genesis_transaction'] = isModeTestNetwork ? GENESIS_TRANSACTION_TEST_NETWORK : GENESIS_TRANSACTION_MAIN_NETWORK;
+        genesisConfig['genesis_shard_id']    = isModeTestNetwork ? GENESIS_SHARD_ID_TEST_NETWORK : GENESIS_SHARD_ID_MAIN_NETWORK;
     }
 
     initialize() {
--- core/genesis/genesis-config.js
+++ core/genesis/genesis-config.js
@@ -1,6 +1,10 @@
 import config from '../config/config';
 
+export const GENESIS_TRANSACTION_MAIN_NETWORK = '2VngVznbdiQ5tqfWqn2NMP8DijqCbLX79Gygo9yYRVFU6iN35h';
+export const GENESIS_TRANSACTION_TEST_NETWORK = 'wFtfcYFJb75bJCGU3HVsPCRg8RDHnMmsRL565BSaeu3aidqQN';
+export const GENESIS_SHARD_ID_MAIN_NETWORK    = 'qGuUgMMVmaCvqrvoWG6zARjkrujGMpzJmpNhBgz1y3RjBG7ZR';
+export const GENESIS_SHARD_ID_TEST_NETWORK    = '2qkemPc9SozT4r8TdcMgyv9DxgE8m4qUkEJ2eBRQLCNP9GD4Vh';
 export default {
-    genesis_transaction: config.MODE_TEST_NETWORK ? 'wFtfcYFJb75bJCGU3HVsPCRg8RDHnMmsRL565BSaeu3aidqQN' : '2VngVznbdiQ5tqfWqn2NMP8DijqCbLX79Gygo9yYRVFU6iN35h',
-    genesis_shard_id   : config.MODE_TEST_NETWORK ? '2qkemPc9SozT4r8TdcMgyv9DxgE8m4qUkEJ2eBRQLCNP9GD4Vh' : 'qGuUgMMVmaCvqrvoWG6zARjkrujGMpzJmpNhBgz1y3RjBG7ZR'
+    genesis_transaction: config.MODE_TEST_NETWORK ? GENESIS_TRANSACTION_TEST_NETWORK : GENESIS_TRANSACTION_MAIN_NETWORK,
+    genesis_shard_id   : config.MODE_TEST_NETWORK ? GENESIS_SHARD_ID_TEST_NETWORK : GENESIS_SHARD_ID_MAIN_NETWORK
 };
