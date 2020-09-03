00e48c247105b3773a403b314984c86778a19bb3
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2020-09-03 21:14:03 +0100

    improve node selection for consensus round.

--- core/wallet/wallet-transaction-consensus.js
+++ core/wallet/wallet-transaction-consensus.js
@@ -354,7 +354,7 @@ export class WalletTransactionConsensus {
 
     _selectNodesForConsensusRound(numberOfNodes = config.CONSENSUS_ROUND_NODE_COUNT, excludeNodeList = []) {
         return new Promise(resolve => {
-            resolve(_.sampleSize(_.difference(_.filter(network.registeredClients, ws => ws.outBound), excludeNodeList), numberOfNodes));
+            resolve(_.sampleSize(_.difference(_.filter(network.registeredClients, ws => ws.outBound || ws.bidirectional), excludeNodeList), numberOfNodes));
         });
     }
 
