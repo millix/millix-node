4d68a19fddff2a0791bdbc70e270fee0436cfc5c
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2020-10-13 01:04:02 +0100

    update migration scripts

--- scripts/migration/schema-update-5.sql
+++ scripts/migration/schema-update-5.sql
@@ -16,6 +16,7 @@ CREATE INDEX IF NOT EXISTS idx_config_create_date ON config (create_date);
 CREATE INDEX IF NOT EXISTS idx_schema_information_create_date ON schema_information (create_date);
 CREATE INDEX IF NOT EXISTS idx_address_version_create_date ON address_version (create_date);
 
+DROP TABLE IF EXISTS object;
 
 CREATE TABLE normalization
 (
@@ -91,6 +92,8 @@ VALUES ('mode_debug', 'AK5rcMMbWw5xIfXVdRVL'),
        ('shard_zero_name', 'rMSuKEh42OZaeVEgzG62'),
        ('key_public', '9MgxVxyXsM2EozHVUZgw');
 
-UPDATE config SET config_id = (SELECT normalization_id FROM normalization WHERE normalization_name = config_name) WHERE config_name IN (SELECT normalization_name FROM normalization WHERE normalization_name = config_name);
+UPDATE config SET config_id = (SELECT normalization_id FROM normalization WHERE normalization_name = config_name) WHERE config_name IN (SELECT normalization_name FROM normalization);
+UPDATE node_attribute SET attribute_type_id = coalesce((SELECT normalization_id FROM normalization WHERE normalization_name = (SELECT attribute_type FROM node_attribute_type WHERE attribute_type_id = node_attribute.attribute_type_id)), attribute_type_id);
+UPDATE node_attribute_type SET attribute_type_id = (SELECT normalization_id FROM normalization WHERE normalization_name = attribute_type) WHERE attribute_type IN (SELECT normalization_name FROM normalization);
 
 COMMIT;
--- scripts/migration/schema-update-8.sql
+++ scripts/migration/schema-update-8.sql
@@ -11,6 +11,8 @@ VALUES ('peer_rotation_more_than_average', 'Z2z0wVCm6Ai1p7OG4MfN'),
        ('peer_rotation_config', 'H2ODFHCxOl1FErIqCDqG'),
        ('shard_zero_name', 'rMSuKEh42OZaeVEgzG62');
 
-UPDATE config SET config_id = (SELECT normalization_id FROM normalization WHERE normalization_name = config_name) WHERE config_name IN (SELECT normalization_name FROM normalization WHERE normalization_name = config_name);
+UPDATE config SET config_id = (SELECT normalization_id FROM normalization WHERE normalization_name = config_name) WHERE config_name IN (SELECT normalization_name FROM normalization);
+UPDATE node_attribute SET attribute_type_id = coalesce((SELECT normalization_id FROM normalization AS N WHERE normalization_name = (SELECT attribute_type FROM node_attribute_type WHERE attribute_type_id = node_attribute.attribute_type_id)), attribute_type_id);
+UPDATE node_attribute_type SET attribute_type_id = (SELECT normalization_id FROM normalization WHERE normalization_name = attribute_type) WHERE attribute_type IN (SELECT normalization_name FROM normalization);
 
 COMMIT;
