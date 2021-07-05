8d946454a306098d9ed07a58796c658aa7cbdd68
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2021-07-05 15:00:59 +0100

    set minimum fees to 1000mlx in the migration script.

--- scripts/migration/schema-update-14.sql
+++ scripts/migration/schema-update-14.sql
@@ -7,9 +7,9 @@ VALUES ('transaction_fee_proxy', 'qTCYsDQzIoVbaX8iIjry'),
        ('transaction_fee_network', '9hJcCunmEibhDgoLHzC8'),
        ('transaction_fee_default', 'eoSDGGFKD3dYfcKF1nFO');
 
-UPDATE config SET config_name='transaction_fee_proxy', config_id='qTCYsDQzIoVbaX8iIjry', value='100' WHERE config_name='transaction_fee_proxy';
+UPDATE config SET config_name='transaction_fee_proxy', config_id='qTCYsDQzIoVbaX8iIjry', value='1000' WHERE config_name='transaction_fee_proxy';
 UPDATE config SET config_name='transaction_fee_network', config_id='9hJcCunmEibhDgoLHzC8', value='0' WHERE config_name='transaction_fee_network';
-UPDATE config SET config_name='transaction_fee_default', config_id='eoSDGGFKD3dYfcKF1nFO', value='100' WHERE config_name='transaction_fee_default';
+UPDATE config SET config_name='transaction_fee_default', config_id='eoSDGGFKD3dYfcKF1nFO', value='1000' WHERE config_name='transaction_fee_default';
 
 UPDATE schema_information SET value = "14" WHERE key = "version";
 
