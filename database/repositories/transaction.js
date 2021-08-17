2a8f43d3def53116e2415c90e60e11f4fb8283fd
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2021-08-17 15:59:46 +0100

    check output status when selecting outputs to use in a new transaction

--- database/repositories/transaction.js
+++ database/repositories/transaction.js
@@ -1890,7 +1890,7 @@ export default class Transaction {
         return new Promise((resolve) => {
             this.database.all('SELECT transaction_output.*, `transaction`.transaction_date FROM transaction_output \
                               INNER JOIN `transaction` ON `transaction`.transaction_id = transaction_output.transaction_id \
-                              WHERE transaction_output.address_key_identifier=? and is_spent = 0 and transaction_output.is_stable = 1 and is_double_spend = 0',
+                              WHERE transaction_output.address_key_identifier=? and is_spent = 0 and transaction_output.is_stable = 1 and is_double_spend = 0 and transaction_output.status != 3',
                 [addressKeyIdentifier], (err, rows) => {
                     resolve(rows);
                 });
