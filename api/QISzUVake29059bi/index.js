08705a495f2181873ff4f2208c4beb8487ab3edb
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2021-08-30 16:23:38 +0100

    add api to reset transaction validation. update api list

---
+++ api/QISzUVake29059bi/index.js
@@ -0,0 +1,30 @@
+import walletUtils from '../../core/wallet/wallet-utils';
+import Endpoint from '../endpoint';
+import database from '../../database/database';
+import server from '../server';
+import wallet from '../../core/wallet/wallet';
+
+
+/**
+ * api reset_transaction_validation
+ */
+class _QISzUVake29059bi extends Endpoint {
+    constructor() {
+        super('QISzUVake29059bi');
+    }
+
+    /**
+     * this API resets transaction validation status
+     * @param app
+     * @param req
+     * @param res
+     * @returns {*}
+     */
+    handler(app, req, res) {
+        wallet.resetTransactionValidationRejected();
+        res.send({success: true});
+    }
+}
+
+
+export default new _QISzUVake29059bi();
