01fac2dabf85b826bf178f478e8872d2f8b0d9fa
Dmitry Khaleev <dkhaleev@gmail.com>
2021-12-02 12:04:12 +0200

    MILLIX-21: get node public IP

---
+++ api/qRHogKQ1Bb7OT4N9/index.js
@@ -0,0 +1,20 @@
+import network from "../../net/network";
+import Endpoint from "../endpoint";
+
+/**
+ * api get_node_public_ip
+ */
+class _qRHogKQ1Bb7OT4N9 extends Endpoint {
+    constructor() {
+        super('qRHogKQ1Bb7OT4N9');
+    }
+
+    handler(app, req, res) {
+        res.send({
+            api_status: 'ok',
+            node_public_ip: network.nodePublicIp
+        })
+    }
+}
+
+export default new _qRHogKQ1Bb7OT4N9();
