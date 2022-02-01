82a30d6e39c2ed64258f33340e4f7cc836ce3c49
Eriksson <erikssonmonteiro@gmail.com>
2022-02-01 17:34:09 +0000

    Feature/cache db queries (#57)
    * cache stats on millix node api
    
    * remove wallet sync by date
    
    * cache results in wallet sync request

--- core/wallet/wallet.js
+++ core/wallet/wallet.js
@@ -434,9 +434,11 @@ class Wallet {
     }
 
     syncWalletTransactions(ws) {
-        if (!this.defaultKeyIdentifier) {
+        if (!this.defaultKeyIdentifier || !!cache.getCacheItem('wallet', 'is_wallet_transaction_synced')) {
             return Promise.resolve();
         }
+
+        cache.setCacheItem('wallet', 'is_wallet_transaction_synced', true, 300000); /* do sync again on a new connection after 5min */
 
         return new Promise(resolve => {
             mutex.lock(['sync-wallet-balance-request'], unlock => {
@@ -940,67 +942,6 @@ class Wallet {
                    });
     }
 
-    _onSyncTransactionByDate(data, ws) {
-        let node         = ws.node;
-        let connectionID = ws.connectionID;
-        const start      = Date.now();
-        mutex.lock(['sync-transaction'], unlock => {
-            eventBus.emit('wallet_event_log', {
-                type   : 'transaction_sync_by_date',
-                content: data,
-                from   : node
-            });
-
-            const beginTimestamp         = data.begin_timestamp;
-            const endTimestamp           = data.end_timestamp;
-            const excludeTransactionList = data.exclude_transaction_id_list;
-
-            database.applyShards((shardID) => {
-                const transactionRepository = database.getRepository('transaction', shardID);
-                return transactionRepository.listTransactions({
-                    transaction_date_end  : endTimestamp,
-                    transaction_date_begin: beginTimestamp
-                });
-            }).then(transactionsByDate => {
-                // let's exclude the list of tx already present in our
-                // peer.
-                const excludeTransactionSet = new Set(excludeTransactionList);
-                const transactions          = _.filter(transactionsByDate, transactionID => !excludeTransactionSet.has(transactionID));
-
-                // get peers' current web socket
-                let ws = network.getWebSocketByID(connectionID);
-                peer.transactionSyncByDateResponse(transactions, ws);
-                console.log(`[wallet] sending transactions sync by date to node ${ws.nodeID} (response time: ${Date.now() - start}ms)`);
-                // unlock here. now on we are going to send missing
-                // transactions to peer.
-                unlock();
-
-                if (transactionsByDate.length === 0) { // no transaction will be synced
-                    return;
-                }
-
-                // get transaction objects
-                async.mapSeries(transactionsByDate, (transaction, callback) => {
-                    database.firstShardZeroORShardRepository('transaction', transaction.shard_id, transactionRepository => {
-                        return new Promise((resolve, reject) => {
-                            transactionRepository.getTransactionObject(transaction.transaction_id)
-                                                 .then(transaction => transaction ? resolve(transactionRepository.normalizeTransactionObject(transaction)) : reject())
-                                                 .catch(() => reject());
-                        });
-                    }).then(transaction => callback(null, transaction));
-                }, (err, transactions) => {
-                    async.eachSeries(transactions, (transaction, callback) => {
-                        // get peers' current web socket
-                        let ws = network.getWebSocketByID(connectionID);
-                        peer.transactionSendToNode(transaction, ws);
-                        setTimeout(() => callback(), 250);
-                    });
-                });
-
-            }).catch(() => unlock());
-        });
-    }
-
     _onSyncTransaction(data, ws) {
         const startTimestamp = Date.now();
         if (data.routing) {
@@ -1181,10 +1122,22 @@ class Wallet {
     }
 
     _onSyncWalletBalance(data, ws) {
+
+        const addressKeyIdentifier = data.address_key_identifier;
+        const cachedTransactions = cache.getCacheItem('wallet', 'wallet_transaction_sync_' + addressKeyIdentifier);
+        if (cachedTransactions) {
+            if(cachedTransactions.length > 0) {
+                peer.walletTransactionSyncResponse(cachedTransactions, ws);
+            }
+            return;
+        }
+        else if (mutex.getKeyQueuedSize(['transaction']) >= (config.NODE_CONNECTION_OUTBOUND_MAX + config.NODE_CONNECTION_INBOUND_MAX)) {
+            return;
+        }
+
         let node         = ws.node;
         let connectionID = ws.connectionID;
         mutex.lock(['sync-wallet-balance'], unlock => {
-            const addressKeyIdentifier = data.address_key_identifier;
             console.log('[wallet] transaction sync for wallet key identifier ', addressKeyIdentifier);
             eventBus.emit('wallet_event_log', {
                 type   : 'wallet_transaction_sync',
@@ -1197,8 +1150,10 @@ class Wallet {
                 console.log('[wallet] >>', transactions.length, ' transaction can be synced to wallet ', addressKeyIdentifier);
                 let ws = network.getWebSocketByID(connectionID);
                 if (transactions && transactions.length > 0 && ws) {
-                    peer.walletTransactionSyncResponse(_.map(transactions, t => t.transaction_id), ws);
+                    transactions = _.map(transactions, t => t.transaction_id);
+                    peer.walletTransactionSyncResponse(transactions, ws);
                 }
+                cache.setCacheItem('wallet', 'wallet_transaction_sync_' + addressKeyIdentifier, transactions, 120000);
                 unlock();
             }).catch(() => unlock());
         }, undefined, Date.now() + config.NETWORK_LONG_TIME_WAIT_MAX * 20);
@@ -1536,11 +1491,9 @@ class Wallet {
         if (this.initialized) {
             this.syncWalletTransactions(ws).then(_ => _);
         }
-        walletSync.doProgressiveSync(ws);
     }
 
     _onPeerConnectionClosed(ws) {
-        walletSync.stopProgressiveSync(ws);
     }
 
     _doShardZeroPruning() {
@@ -1806,7 +1759,6 @@ class Wallet {
                       eventBus.on('transaction_new_proxy', this._onTransactionProxy.bind(this));
                       eventBus.on('transaction_new', this._onNewTransaction.bind(this));
                       eventBus.on('transaction_sync', this._onSyncTransaction.bind(this));
-                      eventBus.on('transaction_sync_by_date', this._onSyncTransactionByDate.bind(this));
                       eventBus.on('transaction_sync_response', this._onTransactionSyncResponse.bind(this));
                       eventBus.on('shard_sync_request', this._onSyncShard.bind(this));
                       eventBus.on('wallet_transaction_sync', this._onSyncWalletBalance.bind(this));
@@ -1896,11 +1848,10 @@ class Wallet {
         eventBus.removeAllListeners('transaction_new_proxy');
         eventBus.removeAllListeners('transaction_new');
         eventBus.removeAllListeners('transaction_sync');
-        eventBus.removeAllListeners('transaction_sync_by_date');
         eventBus.removeAllListeners('transaction_sync_response');
-        eventBus.removeAllListeners('transaction_sync_by_date_response');
         eventBus.removeAllListeners('shard_sync_request');
         eventBus.removeAllListeners('wallet_transaction_sync');
+        eventBus.removeAllListeners('wallet_transaction_sync_response');
         eventBus.removeAllListeners('transaction_validation_request');
         eventBus.removeAllListeners('transaction_validation_response');
         eventBus.removeAllListeners('transaction_spend_request');
