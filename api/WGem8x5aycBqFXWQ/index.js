532873eb2322b0f9cb29811ada8a80284858031a
AShulpekov <ash52131@gmail.com>
2022-03-30 16:24:23 +0300

    MILLIX-134: add endpoint to retreive node version

---
+++ api/WGem8x5aycBqFXWQ/index.js
@@ -0,0 +1,49 @@
+import Endpoint from '../endpoint';
+
+const https = require('https')
+
+/**
+ * api get_latest_millix_version
+ */
+class _WGem8x5aycBqFXWQ extends Endpoint {
+    constructor() {
+        super('WGem8x5aycBqFXWQ');
+    }
+
+    /**
+     * returns a latest millix version
+     * @param app
+     * @param req
+     * @param res
+     */
+    handler(app, req, res) {
+        const options = {
+            hostname: 'millix.org',
+            port: 443,
+            path: '/latest.php',
+            method: 'GET'
+        }
+
+        const request = https.request(options, result => {
+            result.on('data', d => {
+                const buf = Buffer.from(d, 'utf8');
+                res.send({
+                    api_status: 'success',
+                    api_message: buf.toString().replace(/(\n)/gm, "")
+                })
+            })
+        })
+
+        request.on('error', error => {
+            res.send({
+                api_status: 'fail',
+                api_message: error
+            })
+        })
+
+        request.end()
+    }
+}
+
+
+export default new _WGem8x5aycBqFXWQ();
