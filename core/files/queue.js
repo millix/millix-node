6e26d304119fc27b5ccac3e9988d4e3e7f9e363f
João Rafael Almeida <joao.rafael.almeida@ua.pt>
2022-03-30 22:43:45 +0100

    Refactoring in sender and queue

--- core/files/queue.js
+++ core/files/queue.js
@@ -6,6 +6,9 @@ import console from '../console';
 import config from '../config/config';
 import async from 'async';
 
+const SENDER   = 'sender';
+const RECEIVER = 'receiver';
+
 
 class Queue {
     constructor() {
@@ -23,7 +26,7 @@ class Queue {
             if (!fs.existsSync(this.filesPendingToSend)) {
                 fs.closeSync(fs.openSync(this.filesPendingToSend, 'w'));
             }
-            this.listOfPendingFilesInSender = this._loadPendingFiles(this.filesPendingToSend);
+            this._loadPendingFilesFromSender(this.filesPendingToSend);
             resolve();
         });
     }
@@ -33,16 +36,37 @@ class Queue {
             if (!fs.existsSync(this.filesPendingToReceive)) {
                 fs.closeSync(fs.openSync(this.filesPendingToReceive, 'w'));
             }
-            this.listOfPendingFilesInReceiver = this._loadPendingFiles(this.filesPendingToReceive);
+            this._loadPendingFilesFromReceiver(this.filesPendingToReceive);
             resolve();
         });
     }
 
-    _buildEntry(nodeId, transactionId, fileHash, nodePublicKey) {
-        return nodeId + ';' + transactionId + ';' + fileHash + ';' + nodePublicKey + '\n';
+    _buildEntry(entry, side) {
+        if (side === SENDER) {
+            return this._buildEntryForSender(entry);
+        }
+        else if (side === RECEIVER) {
+            return this._buildEntryForReceiver(entry);
+        }
     }
 
-    _writeInFile(data, fileLocation) {
+    _buildEntryForSender(entry) {
+        return entry.nodeId + ';' +
+               entry.transactionId + ';' +
+               entry.fileHash + ';' +
+               entry.nodePublicKey + '\n';
+    }
+
+    _buildEntryForReceiver(entry) {
+        return entry.nodeId + ';' +
+               entry.transactionId + ';' +
+               entry.fileHash + ';' +
+               entry.nodePublicKey + ';' +
+               entry.numberOfChunks + ';' +
+               entry.requestedChunk + '\n';
+    }
+
+    _writeInFile(data, fileLocation, side) {
         return new Promise((resolve, reject) => {
             fs.writeFile(fileLocation, '', (err) => {
                 if (err) {
@@ -50,7 +74,7 @@ class Queue {
                     return reject();
                 }
                 async.forEachOf(data, (entry, index, next) => {
-                    let line = this._buildEntry(entry.nodeId, entry.transactionId, entry.fileHash, entry.nodePublicKey);
+                    let line = this._buildEntry(entry, side);
                     fs.appendFile(fileLocation, line, (err) => {
                         if (err) {
                             console.log('[file-queue] error, ', err);
@@ -61,24 +85,6 @@ class Queue {
                 }, () => resolve());
             });
         });
-    }
-
-    _loadPendingFiles(fileLocation) {
-        let listOfPendingFiles = [];
-        mutex.lock(['update_pending_files'], (unlock) => {
-            let content = fs.readFile(fileLocation);
-            content.split(/\r?\n/).forEach(line => {
-                let elements = line.split(';');
-                listOfPendingFiles.append({
-                    nodeId       : elements[0],
-                    transactionId: elements[1],
-                    fileHash     : elements[2],
-                    nodePublicKey: elements[3]
-                });
-            });
-            unlock();
-        });
-        return listOfPendingFiles;
     }
 
     /***********************
@@ -127,7 +133,7 @@ class Queue {
             this.listOfPendingFilesInSender = this.listOfPendingFilesInSender.filter(function(value, index, arr) {
                 return nodeId !== value.nodeId && transactionId !== value.transactionId;
             });
-            this._writeInFile(this.listOfPendingFilesInSender, this.filesPendingToSend).then(() => {
+            this._writeInFile(this.listOfPendingFilesInSender, this.filesPendingToSend, SENDER).then(() => {
                 unlock();
             });
         });
@@ -143,6 +149,22 @@ class Queue {
         return (this.listOfPendingFilesInSender.filter((value, index, arr) => {
             return nodeId === value.nodeId && transactionId === value.transactionId;
         }).length > 0);
+    }
+
+    _loadPendingFilesFromSender(fileLocation) {
+        mutex.lock(['update_pending_files'], (unlock) => {
+            let content = fs.readFile(fileLocation);
+            content.split(/\r?\n/).forEach(line => {
+                let elements = line.split(';');
+                this.listOfPendingFilesInSender.append({
+                    nodeId       : elements[0],
+                    transactionId: elements[1],
+                    fileHash     : elements[2],
+                    nodePublicKey: elements[3]
+                });
+            });
+            unlock();
+        });
     }
 
     /***********************
@@ -166,7 +188,7 @@ class Queue {
         return this.listOfPendingFilesInReceiver;
     }
 
-    addNewFileInReceiver(nodeId, transactionId, fileHash, nodePublicKey) {
+    addNewChunkInReceiver(nodeId, transactionId, fileHash, nodePublicKey, numberOfChunks, chunkNumber) {
         let newEntry = this._buildEntry(nodeId, transactionId, fileHash, nodePublicKey);
         mutex.lock(['update_pending_files'], (unlock) => {
             fs.appendFile(this.filesPendingToReceive, newEntry, (err) => {
@@ -175,10 +197,12 @@ class Queue {
                 }
                 else {
                     this.listOfPendingFilesInReceiver.append({
-                        nodeId       : nodeId,
-                        transactionId: transactionId,
-                        fileHash     : fileHash,
-                        nodePublicKey: nodePublicKey
+                        nodeId        : nodeId,
+                        transactionId : transactionId,
+                        fileHash      : fileHash,
+                        nodePublicKey : nodePublicKey,
+                        numberOfChunks: numberOfChunks,
+                        chunkNumber   : chunkNumber
                     });
                 }
                 unlock();
@@ -186,17 +210,47 @@ class Queue {
         });
     }
 
-    removeEntryFromReceiver(nodeId, transactionId) {
+    removeEntryFromReceiver(nodeId, transactionId, fileHash, chunkNumber) {
         mutex.lock(['update_pending_files'], (unlock) => {
             this.listOfPendingFilesInReceiver = this.listOfPendingFilesInReceiver.filter(function(value, index, arr) {
-                return nodeId !== value.nodeId && transactionId !== value.transactionId;
+                return nodeId !== value.nodeId && transactionId !== value.transactionId && transactionId !== value.fileHash && chunkNumber !== value.chunkNumber;
             });
-            this._writeInFile(this.listOfPendingFilesInReceiver, this.filesPendingToReceive).then(() => {
+            this._writeInFile(this.listOfPendingFilesInReceiver, this.filesPendingToReceive, RECEIVER).then(() => {
                 unlock();
             });
         });
     }
 
+    hasChunkToReceive(nodeId, transactionId, fileHash, chunkNumber) {
+        return (this.listOfPendingFilesInReceiver.filter((value, index, arr) => {
+            return nodeId === value.nodeId && transactionId === value.transactionId && fileHash === value.fileHash && chunkNumber === value.chunkNumber;
+        }).length > 0);
+    }
+
+    isLastChunk(nodeId, transactionId, fileHash, chunkNumber) {
+        let file = this.listOfPendingFilesInReceiver.filter((value, index, arr) => {
+            return nodeId === value.nodeId && transactionId === value.transactionId && fileHash === value.fileHash && chunkNumber === value.chunkNumber;
+        });
+        return file[0].numberOfChunks === chunkNumber;
+    }
+
+    _loadPendingFilesFromReceiver(fileLocation) {
+        mutex.lock(['update_pending_files'], (unlock) => {
+            let content = fs.readFile(fileLocation);
+            content.split(/\r?\n/).forEach(line => {
+                let elements = line.split(';');
+                this.listOfPendingFilesInReceiver.append({
+                    nodeId        : elements[0],
+                    transactionId : elements[1],
+                    fileHash      : elements[2],
+                    nodePublicKey : elements[3],
+                    numberOfChunks: elements[4],
+                    requestedChunk: elements[5]
+                });
+            });
+            unlock();
+        });
+    }
 }
 
 
--- core/files/receiver.js
+++ core/files/receiver.js
@@ -10,20 +10,22 @@ import https from 'https';
 import walletUtils from '../wallet/wallet-utils';
 import queue from './queue';
 import request from 'request';
+import async from 'async';
+import network from '../../net/network';
 
-class Receiver{
-    constructor(){
-        this.isSenderPublic = false;
-        this.filesRootFolder = path.join(os.homedir(), config.FILES_CONNECTION.FOLDER);
-        this.isSenderPublic  = true;
-        this.serverOptions   = {};
-        this.httpsServer     = null;
-        this.app             = null;
+
+class Receiver {
+    constructor() {
+        this.serverOptions = {};
+        this.httpsServer   = null;
+        this.app           = null;
+        this.nodeId        = null;
     }
 
     initialize() {
         return new Promise((resolve, reject) => {
-            this._setUpApp();
+            this.nodeId = network.nodeID;
+            this._defineServerOperations();
             walletUtils.loadNodeKeyAndCertificate()
                        .then(({
                                   certificate_private_key_pem: certificatePrivateKeyPem,
@@ -38,133 +40,113 @@ class Receiver{
                            };
                            resolve();
                        }).then(() => {
-                queue.initializeReceiver().then(() => {
-                    const promisesToSend = queue.getListOfPendingFilesInReceiver().rows.map(requestInfo => new Promise((resolve, reject) => {
-                        //to do this._st(resolve, reject, requestInfo);
-                    }));
-                    Promise.all(promisesToSend)
-                           .then(() => {
-                               resolve();
-                           });
-                });
+                queue.initializeReceiver()
+                     .then(() => {
+                         resolve();
+                     });
             });
         });
     }
 
-    receive(server, requestInfo){
-        return new Promise((resolve, reject) => {
-            if (!this.isSenderPublic && !requestInfo.receiver_public) {
-             console.log('Both peers are private! Cannot communicate!');
-             reject();
-             }
-
-            if (this.isSenderPublic) {
-                this._receiveFromPublicServer(server, requestInfo)
-                    .then(()=>{
-                        resolve();
-                    }).catch(e => {
-                    console.log('error');
-                    reject();
-                });
-            }
-            else {
-                this._receiveFromPrivateServer(server, requestInfo)
-                    .then(()=>{
-                        resolve();
-                    }).catch(e => {
-                    console.log('error');
-                    reject();
-                });
-            }
-        });
+    getPublicReceiverInfo() {
+        if (!queue.anyActiveReceiverServer()) {
+            this.httpsServer = https.createServer(this.serverOptions, this.app).listen(0);
+            console.log('[file-receiver] Server listening on port ' + this.httpsServer.address().port);
+        }
+        queue.incrementServerInstancesInReceiver();
+        return this.httpsServer;
     }
 
-    _receiveFromPublicServer(server, requestedFiles){
-        return new Promise((resolve, reject) => {
-            const wallet = requestedFiles.wallet;
-            const transactionId = requestedFiles.transaction;
-            const promisesToReceive = requestedFiles.files.map(file => new Promise((resolve, reject) => {
-                for (let chunk = 0; chunk < file.chunks; chunk++){
-                    const service = server.concat("/file/").concat(wallet).concat("/").concat(transactionId).concat("/").concat(file.name).concat("/").concat(chunk);
-                    console.log(service)
-                    request.get(service, (error, response, body) => {
-                        if(error) {
-                            console.log("error")
-                            reject();
-                        }
-                        chunker.writeFile(wallet, transactionId, file.name, body);
-                    });
-                }
-                resolve();
-            }));
-
-            Promise.all(promisesToReceive)
-                   .then(() => {
-                       return new Promise((resolve, reject) => {
-                           const service = server.concat("/ack");
-                           request.post(service, (error, response, body) => {
-                               if(error) {
-                                   console.log("error")
-                                   reject();
-                               }
-                               resolve();
-                           });
-                       });
-                   })
-                   .then(() => {
-                       resolve();
-                   });
-        });
-    }
-
-    _receiveFromPrivateServer(server, requestInfo){
-        return new Promise((resolve, reject) => {
-            console.log("eeee");
-            resolve();
-        });
-    }
-
-    _setUpApp(){
-        let filesRootFolder = this.filesRootFolder;
+    _defineServerOperations() {
         this.app = express();
         this.app.use(helmet());
         this.app.use(bodyParser.json({limit: '50mb'}));
         this.app.use(cors());
 
-        this.app.post("/file/:wallet/:txid/:fname/:chunkn", (req, res) => {
-            let wallet = req.params.wallet;
-            let transactionId = req.params.txid;
-            let fileName = req.params.fname;
-            let chunkNumber = req.params.chunkn;
+        this.app.post('/file/:nodeId/:addressKeyIdentifier/:transactionId/:fileHash/:chunkNumber', (req, res) => {
+            let nodeId               = req.params.nodeId;
+            let addressKeyIdentifier = req.params.addressKeyIdentifier;
+            let transactionId        = req.params.transactionId;
+            let fileHash             = req.params.fileHash;
+            let chunkNumber          = req.params.chunkNumber;
 
-            console.log(req.body);
-            //HERE
-
-            /*let location = path.join(filesRootFolder, wallet);
-             location = path.join(location, transactionId);
-             location = path.join(location, fileName);
-
-             res.writeHead(200);
-             chunker.getChunck(res, location, chunkNumber);*/
+            if (queue.hasChunkToReceive(nodeId, transactionId, fileHash, chunkNumber)){
+                let chunk = req.body.chunk;
+                chunker.writeFile(addressKeyIdentifier, transactionId, fileHash, chunk);
+                if(queue.isLastChunk(nodeId, transactionId, fileHash, chunkNumber)){
+                    queue.decrementServerInstancesInReceiver();
+                    queue.removeEntryFromSender(nodeId, transactionId);
+                }
+                res.writeHead(200);
+                res.end('ok');
+            } else {
+                res.writeHead(403);
+                res.end('Requested file is not in queue to be send!');
+            }
         });
+    }
 
-        this.app.post("/ack", function (req, res) {
-            //Check hash
-            queue.decrementServerInstancesInReceiver();
-            res.writeHead(200);
-            res.end("ok");
+    registerFileChunk(nodeId, transactionId, fileHash, nodePublicKey, numberOfChunks, requestedChunk) {
+        queue.addNewChunkInReceiver(nodeId, transactionId, fileHash, nodePublicKey, numberOfChunks, requestedChunk);
+    }
+
+    receive(server, requestedFiles) {
+        return new Promise((resolve, reject) => {
+            const self                 = this;
+            const addressKeyIdentifier = requestedFiles.addressKeyIdentifier;
+            const transactionId        = requestedFiles.transactionId;
+            const promisesToReceive    = requestedFiles.files.map(file => new Promise((resolve, reject) => {
+                async.times(file.chunks, (chunkNumber, next) => {
+                    const service = server.concat('/file/')
+                                          .concat(self.nodeId).concat('/')
+                                          .concat(addressKeyIdentifier).concat('/')
+                                          .concat(transactionId).concat('/')
+                                          .concat(file.name).concat('/')
+                                          .concat(chunkNumber);
+                    request.get(service, (err, response, body) => {
+                        if (err) {
+                            console.log('[file-receiver] error, ', err);
+                            return next(err);
+                        }
+                        chunker.writeFile(wallet, transactionId, file.name, body).then(() => {
+                            next();
+                        }).catch((err) => {
+                            return next(err);
+                        });
+                    });
+                }, (err) => {
+                    if (err) {
+                        return reject();
+                    }
+                    return resolve();
+                });
+
+            }));
+
+            Promise.all(promisesToReceive)
+                   .then(() => {
+                       return new Promise((resolve, reject) => {
+                           const service = server.concat('/ack/')
+                                                 .concat(self.nodeId).concat('/')
+                                                 .concat(transactionId).concat('/');
+                           request.post(service, (err, response, body) => {
+                               if (err) {
+                                   console.log('[file-receiver] error, ', err);
+                                   return reject();
+                               }
+                               resolve();
+                           });
+                       });
+                   }).then(() => {
+                resolve();
+            }).catch((err) => {
+                return reject(err);
+            });
         });
     }
 
 
-    getPublicReceiverInfo(){
-        if(!queue.anyActiveReceiverServer()){
-            this.httpsServer = https.createServer(this.serverOptions, this.app).listen(0);
-            console.log('Listening on port ' + this.httpsServer.address().port);
-        }
-        queue.incrementServerInstancesInReceiver();
-        return this.httpsServer;
-    }
 }
+
 
 export default new Receiver();
