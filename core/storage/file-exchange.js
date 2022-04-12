8cd1a4c75b8ac8faf5f872cc2d99cc6384b58da8
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2022-04-12 13:09:12 +0100

    update file exchange module

--- core/storage/file-exchange.js
+++ core/storage/file-exchange.js
@@ -1,35 +1,20 @@
-import WebSocket, {Server} from 'ws';
 import path from 'path';
 import os from 'os';
 import fs from 'fs';
-import crypto from 'crypto';
-import wallet from '../wallet/wallet';
-import mutex from '../mutex';
-import console from '../console';
-import database from '../../database/database';
-import config, {NODE_BIND_IP} from '../config/config';
-import https from 'https';
-import walletUtils from '../wallet/wallet-utils';
-import base58 from 'bs58';
+import config from '../config/config';
 import sender from './sender';
+import receiver from './receiver';
+import fileManager from './file-manager';
+import _ from 'lodash';
+import async from 'async';
+import peer from '../../net/peer';
+import network from '../../net/network';
+import eventBus from '../event-bus';
 
-const WebSocketServer = Server;
-
-/**
- * Open server to listen what to do
- * Receives message to send:
- *      * Args:
- *          - Destination (address and port)
- *          - chunk (optional?)
- *      * Return:
- *          - 1 it: num of Chunks
- *          - then: chunks
- * */
 
 class FileExchange {
     constructor() {
-        this.filesRootFolder = null;
-        this.walletKeyIdentifier = null;
+        this.activeTransactionSync = new Set();
     }
 
     initialize() {
@@ -37,37 +22,152 @@ class FileExchange {
         if (!fs.existsSync(this.filesRootFolder)) {
             fs.mkdirSync(path.join(this.filesRootFolder));
         }
-        this.walletKeyIdentifier = wallet.getKeyIdentifier();
-        this._startServiceForMessageExchange();
+        return sender.initialize()
+                     .then(() => receiver.initialize())
+                     .then(() => {
+                         this._registerEventListeners();
+                     });
     }
 
-    _startServiceForMessageExchange(){
+    _onTransactionFileSyncRequest(data, ws) {
+        const {
+                  address_key_identifier: addressKeyIdentifier,
+                  transaction_id        : transactionID,
+                  transaction_file_list : transactionFileList
+              }                 = data;
+        const fileAvailableList = [];
+        async.eachSeries(transactionFileList, (fileHash, callback) => {
+            fileManager.hasFile(addressKeyIdentifier, transactionID, fileHash)
+                       .then(exists => {
+                           if (exists) {
+                               return sender.getNumberOfChunks(addressKeyIdentifier, transactionID, fileHash)
+                                            .then(totalChunks => {
+                                                fileAvailableList.push({
+                                                    name       : fileHash,
+                                                    chunk_count: totalChunks
+                                                });
+                                                callback();
+                                            }).catch(() => callback());
+                           }
+                           callback();
+                       });
+        }, () => {
+            if (fileAvailableList.length === 0) {
+                return peer.transactionFileSyncResponse(transactionID, {transaction_file_not_found: true}, ws);
+            }
+
+            const data = {
+                transaction_id        : transactionID,
+                address_key_identifier: addressKeyIdentifier,
+                transaction_file_list : fileAvailableList
+            };
+
+            if (network.nodeIsPublic) {
+                const server            = sender.newSenderInstance();
+                data['server_endpoint'] = `https://${network.nodePublicIp}:${server.address().port}/`;
+                const filesToRemove     = [];
+
+                async.eachSeries(fileAvailableList, (file, callback) => { // serve files via https server
+                    sender.serveFile(ws.nodeID, addressKeyIdentifier, transactionID, file.file_hash)
+                          .then(() => callback())
+                          .catch(() => {
+                              filesToRemove.push(file);
+                              callback();
+                          });
+                }, () => {
+                    _.pull(fileAvailableList, filesToRemove);
+                    return peer.transactionFileSyncResponse(data, ws);
+                });
+            }
+            else {
+                return peer.transactionFileSyncResponse(data, ws); /* node not public:  no server  endpoint */
+            }
+        });
+
+    }
+
+    _onTransactionFileChunkRequest(data) {
+        return sender.sendChunk(data.receiver_endpoint, data.address_key_identifier, data.transaction_id, data.file_hash, data.chunk_number);
+    }
+
+    _registerEventListeners() {
         // message exchange protocol here
+        eventBus.on('transaction_file_chunk_request', this._onTransactionFileChunkRequest.bind(this));
+        eventBus.on('transaction_file_request', this._onTransactionFileSyncRequest.bind(this));
     }
 
-    exchange(){
-        //where i will call this?
-        //to do: Exchange of messages to decide if the node is interested in receiving the file.
-        let senderAddress = sender.getAddress();
-        //Expected format
-        const transactionToExchange = {
-            wallet:"muo5n6eDKj2t9BhdXZWBkEed75zi4ZkMqY",
-            transaction:"123transaction321",
-            files:[
-                {
-                    name:"transactionAttributes.json",
-                    chunks:1
-                },
-                {
-                    name:"c93e8a5a2d7c11998bccc1d0fa55e2b5fdfaeaa18a2e439bb199b3b5f2b700fd",
-                    chunks:1
-                },
-                {
-                    name:"7cb3a19fc765cf66287c372aa7c5979495e15c56afc1c1ef4cdfc448cb220561",
-                    chunks:1
-                }
-            ]
-        };
+    syncFilesFromTransaction(transaction) {
+        const transactionId        = transaction.transaction_id,
+              addressKeyIdentifier = transaction.transaction_input_list[0].address_key_identifier,
+              fileList             = transaction.transaction_output_attribute.transaction_output_metadata.files;
+
+        if (!transactionId || !addressKeyIdentifier || !fileList ||
+            this.activeTransactionSync.has(transactionId)) {
+            return;
+        }
+
+        this.activeTransactionSync.add(transactionId);
+
+        const fileListToRequest = [];
+        async.eachSeries(fileList, (file, callback) => {
+            fileManager.hasFile(addressKeyIdentifier, transactionId, file.name)
+                       .then(hasFile => {
+                           if (hasFile) {
+                               return callback();
+                           }
+                           fileListToRequest.push(file);
+                           callback();
+                       });
+        }, () => {
+            if (fileListToRequest.length > 0) {
+                let nodesWS = _.shuffle(network.registeredClients);
+                async.eachSeries(nodesWS, (ws, callback) => {
+                    peer.transactionFileSyncRequest(addressKeyIdentifier, transactionId, fileListToRequest, ws)
+                        .then((data) => {
+                            let serverEndpoint = data.server_endpoint;
+                            if (serverEndpoint) {
+                                receiver.downloadFileList(serverEndpoint, data.transaction_file_list)
+                                        .then(() => callback(true))
+                                        .catch(({files_downloaded: filesDownloaded}) => {
+                                            if (filesDownloaded.length > 0) {
+                                                _.remove(fileListToRequest, file => filesDownloaded.has(file.name));
+                                            }
+
+                                            if (fileListToRequest.length === 0) { // no more files to request
+                                                return callback(true);
+                                            }
+
+                                            callback();
+                                        });
+                            }
+                            else {
+                                if (!network.nodeIsPublic) {
+                                    return callback();
+                                }
+
+                                receiver.requestFileListUpload(serverEndpoint, addressKeyIdentifier, transactionId, data.transaction_file_list, ws)
+                                        .then(() => callback(true))
+                                        .catch(({files_received: filesReceived}) => {
+                                            if (filesReceived.length > 0) {
+                                                _.remove(fileListToRequest, file => filesReceived.has(file.name));
+                                            }
+
+                                            if (fileListToRequest.length === 0) { //no more files to request
+                                                return callback(true);
+                                            }
+
+                                            callback();
+                                        });
+                            }
+                        }).catch(() => callback());
+                }, () => {
+                    this.activeTransactionSync.delete(transactionId);
+                });
+            }
+            else {
+                this.activeTransactionSync.delete(transactionId);
+            }
+        });
     }
 
 }
