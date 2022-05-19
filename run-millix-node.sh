495e718237e5b165d669d3fe5e98343fa2229345
crank <alex.olkhovskii@404publishing.com>
2022-05-19 17:38:46 +0200

    update sqlite3 run-millix-node.sh - use babel-node from node_modules instead of global

--- run-millix-node.sh
+++ run-millix-node.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ip_address=127.0.0.1
 echo "running on $ip_address"
-babel-node --max-old-space-size=2048 index.js --host $ip_address --port 10000 --api-port 5500 --debug true --data-folder ./millix/
+./node_modules/@babel/node/bin/babel-node.js --max-old-space-size=2048 index.js --host $ip_address --port 10000 --api-port 5500 --debug true --data-folder ./millix/
