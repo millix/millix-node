234b53fb8f7b39750732348bb7af9f457311f24b
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2022-10-05 02:02:48 +0100

    bump version 1.21.5

--- core/config/config.js
+++ core/config/config.js
@@ -1,8 +1,8 @@
 const const_value_default = {
     'MODE_DEBUG'                : false,
     'MODE_TEST_NETWORK'         : false,
-    'NODE_MILLIX_BUILD_DATE'    : 1664207584,
-    'NODE_MILLIX_VERSION'       : '1.21.4',
+    'NODE_MILLIX_BUILD_DATE'    : 1664816702,
+    'NODE_MILLIX_VERSION'       : '1.21.5',
     'DATA_BASE_DIR_MAIN_NETWORK': './millix',
     'DATA_BASE_DIR_TEST_NETWORK': './millix-testnet',
     'DEBUG_LOG_FILTER'          : [],
