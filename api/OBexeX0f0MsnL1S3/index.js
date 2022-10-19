7483bc1f2297ced8d6a5496e326bd63a7e4fc878
millix-alex <57537379+millix-alex@users.noreply.github.com>
2022-10-19 14:44:16 +0200

    get session - return private_key_exists flag (#99)
    Co-authored-by: crank <alex.olkhovskii@404publishing.com>

--- api/OBexeX0f0MsnL1S3/index.js
+++ api/OBexeX0f0MsnL1S3/index.js
@@ -2,6 +2,7 @@ import Endpoint from '../endpoint';
 import wallet from '../../core/wallet/wallet';
 import database from '../../database/database';
 import _ from 'lodash';
+import WalletUtils from '../../core/wallet/wallet-utils';
 
 
 /**
@@ -21,27 +22,37 @@ class _OBexeX0f0MsnL1S3 extends Endpoint {
      */
     handler(app, req, res) {
         if (!wallet.initialized || _.isEmpty(wallet.getActiveWallets())) {
-            return res.send({
-                api_status : 'fail',
-                api_message: 'wallet not loaded'
+            let resultError = {
+                api_status        : 'fail',
+                api_message       : 'wallet not loaded',
+                private_key_exists: false
+            };
+
+            WalletUtils.loadMnemonic().then(() => {
+                resultError.private_key_exists = true;
+
+                res.send(resultError);
+            }).catch(e => {
+                res.send(resultError);
             });
         }
-
-        const walletID      = wallet.getDefaultActiveWallet();
-        const keyIdentifier = wallet.defaultKeyIdentifier;
-        database.getRepository('address').getAddressBaseAttribute(keyIdentifier, 'key_public')
-                .then(publicKey => {
-                    const addressVersion = database.getRepository('address').getDefaultAddressVersion().version;
-                    res.send({
-                        api_status: 'success',
-                        wallet    : {
-                            id                    : walletID,
-                            address               : `${keyIdentifier}${addressVersion}${keyIdentifier}`,
-                            address_key_identifier: keyIdentifier,
-                            address_public_key    : publicKey
-                        }
+        else {
+            const walletID      = wallet.getDefaultActiveWallet();
+            const keyIdentifier = wallet.defaultKeyIdentifier;
+            database.getRepository('address').getAddressBaseAttribute(keyIdentifier, 'key_public')
+                    .then(publicKey => {
+                        const addressVersion = database.getRepository('address').getDefaultAddressVersion().version;
+                        res.send({
+                            api_status: 'success',
+                            wallet    : {
+                                id                    : walletID,
+                                address               : `${keyIdentifier}${addressVersion}${keyIdentifier}`,
+                                address_key_identifier: keyIdentifier,
+                                address_public_key    : publicKey
+                            }
+                        });
                     });
-                });
+        }
     }
 }
 
